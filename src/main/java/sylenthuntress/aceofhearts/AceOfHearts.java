2780a17dae9cc361ace9db65a2e7941669aa8096
SylentHuntress <GaymerGirlEva@gmail.com>
2025-06-02 01:05:49 -0500

    Initial commit

---
+++ src/main/java/sylenthuntress/aceofhearts/AceOfHearts.java
@@ -0,0 +1,20 @@
+package sylenthuntress.aceofhearts;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AceOfHearts implements ModInitializer {
+    public static final String MOD_ID = "aceofhearts";
+    public static final String MOD_NAME = "AceOfHearts";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+    public static Identifier modIdentifier(String id) {
+        return Identifier.of(MOD_ID, id);
+    }
+
+    public void onInitialize() {
+        ExampleMod.LOGGER.info(MOD_NAME + " by SylentHuntress successfully loaded!");
+    }
+}
